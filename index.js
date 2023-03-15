662e9eb1fb858eead84456c56a7ee51b1d588e2b
darkprince131 <piladebajyoti131@gmail.com>
2023-03-15 16:06:56 +0530

    changes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const router = express.Router();
 const path = require("path");
+const PORT = process.env.PORT || 8800
 
 dotenv.config();
 
@@ -49,6 +50,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(process.env.PORT || 8800, () => {
+app.listen(PORT, () => {
   console.log("Backend server is running!");
 });
